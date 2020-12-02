9424b1ed73ea1d50ce324ad5619e46d468e988fb
Valerio Di Punzio <sayhi@valeriodipunzio.com>
2020-12-02 18:07:43 +0100

    removed console log

--- src/modules/emitCustomEvent/emitCustomEvent.js
+++ src/modules/emitCustomEvent/emitCustomEvent.js
@@ -6,7 +6,6 @@ export default ( elem, eventName, eventOptions ) => {
     eventOptions = mergeObjects({ bubbles: true }, eventOptions);
     const eventObj = new CustomEvent(eventName, eventOptions);
     getElements(elem).forEach($el => {
-        console.log('emitEvent',$el);
         $el.dispatchEvent( eventObj );
     });
 }
--- src/modules/emitEvent/emitEvent.js
+++ src/modules/emitEvent/emitEvent.js
@@ -6,7 +6,6 @@ export default ( elem, eventName, eventOptions ) => {
     eventOptions = mergeObjects({ bubbles: true }, eventOptions);
     const eventObj = new Event(eventName, eventOptions);
     getElements(elem).forEach($el => {
-        console.log('emitEvent',$el);
         $el.dispatchEvent( eventObj );
     });
 }
